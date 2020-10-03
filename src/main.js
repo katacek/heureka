519c16d21a2670a586d746421a7c2e2d5b7d90c2
katacek <katacek@gmail.com>
2020-10-03 17:12:50 +0200

    heureka

--- src/main.js
+++ src/main.js
@@ -1,23 +1,31 @@
 const Apify = require('apify');
 const { handleStart, handleList, handleDetail } = require('./routes');
 
-
 const { utils: { log } } = Apify;
 
 Apify.main(async () => {
- 
+    //const { startUrls } = await Apify.getInput();
+
+    //const requestList = await Apify.openRequestList('start-urls', startUrls);
     const requestQueue = await Apify.openRequestQueue();
-    await requestQueue.addRequest({ url: "https://www.okay.sk/" });
+    await requestQueue.addRequest({ url: "https://pletova-kosmetika.heureka.cz/" });
 
     const crawler = new Apify.CheerioCrawler({
+       // requestList,
         requestQueue,
-        useApifyProxy: true,
+        //useApifyProxy: true,
         useSessionPool: true,
         persistCookiesPerSession: true,
-        maxConcurrency: 5,
+        // Be nice to the websites.
+        // Remove to unleash full power.
+        maxConcurrency: 50,
         handlePageTimeoutSecs:600,
        
+       // context is made up by crawler, it contains $, page body, request url, response and session
         handlePageFunction: async (context) => {
+            // from context.request get url and put it to const url (alias url = context.request.url)
+            // moreover, get userdata, and from them get label and put it to label 
+            // alias (label = context.request.userData.label)
             const { url, userData: { label } } = context.request;
             console.log('Page opened.', { label, url });
             log.info('Page opened.', { label, url });
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 const Apify = require('apify');
 
+//const { utils: { log } } = Apify;
+
+// at this point, the main page is already loaded in $
 exports.handleStart = async ({ $ }) =>
 {
     const requestQueue = await Apify.openRequestQueue();
-    const links = $('ul.box-menu__line').find('li.box-menu__item:not(.box-menu__item--title)').find('a.box-menu__item__link').map(function ()
+    //start page, add all categories links to requestQueue
+    const links = $( "a[data-type='subcategory']" ).map(function ()
     { return $(this).attr('href'); }).get();
     for (let link of links)
     {   
+        // request is an object, setting url to link and in userdata, setting new dictionary label: LIST
+        // it is me who is setting the label value, just using it for making the crawler fcn more clear
         await requestQueue.addRequest({
             url: link,
             userData: { label: 'LIST' }
@@ -18,7 +24,8 @@ exports.handleStart = async ({ $ }) =>
 exports.handleList = async ({ $ }) =>
 {
     const requestQueue = await Apify.openRequestQueue();
-    const links = $('li.js-gtm-product-wrapper').find('.title').find('a.js-gtm-product-link').map(function ()
+    //add detail pages of all products on the page to requestQueue
+    const links = $( ".c-product__link" ).map(function ()
     { return $(this).attr('href'); }).get();
     for (let link of links)
     {
@@ -28,42 +35,28 @@ exports.handleList = async ({ $ }) =>
         });
     }
 
-    const nextLink = $('a.next').attr('href');
-    if (nextLink)
-    {
-        await requestQueue.addRequest({
-            url: nextLink,
-            userData: { label: 'LIST' }
-        });
-    }
+    //add next page to requestQueue, if exists
+    // TODO - loaded dynamically by addinf number to url
+    // const nextLink = $('a.next').attr('href');
+    // if (nextLink)
+    // {
+    //     await requestQueue.addRequest({
+    //         url: nextLink,
+    //         userData: { label: 'LIST' }
+    //     });
+    // }
     
 };
 
 exports.handleDetail = async ({ request, $ }) => {
-
-    let productDescription = JSON.parse($('div#page-product-detail').children().first().attr('data-product'));
-
+    //parse detail page
 
     let result = {};
     result.itemUrl = request.url;
-    result.itemId = productDescription.id;
-    result.itemName = $('.product-title.js-productTitle').text().trim();
-    result.currentPrice = parseFloat($('#product_price_wv').text().replace(/\s/g, '').replace(',', '.'));
-    result.originalPrice = parseFloat($('#product_price_recomended').text().replace(/\s/g, '').replace(',', '.'));
-    let additionalDiscount = productDescription.labels.find(x => x.includes('ZĽAVA'));
-    if (additionalDiscount)
-    {
-        additionalDiscount = parseFloat(additionalDiscount.replace('ZĽAVA','').trim());
-        if (additionalDiscount)
-            result.currentPrice = Math.round(result.currentPrice * ((100 - additionalDiscount) / 100)* 100) / 100;
-    }
-    if (!result.originalPrice) result.originalPrice = result.currentPrice;
-    result.discounted = result.currentPrice < result.originalPrice;
-    result.breadcrumb = $('p#menu-breadcrumb').text().trim().split('OKAY »')[1];
-    result.currency = "EUR";
-    result.inStock = !!$('p#availability:contains(kus)').text();
-    result.img = $('a#js-zoomingLinkGallery').attr('href');
-    result.vatInfo = $('.price-highlight.price-name').text();
-
+    result.itemName = $("h1[itemprop='name']").text().trim();
+    result.currentPrice = parseInt($("span[itemprop='price']").text());
+    result.breadcrumb = $('#breadcrumbs').text().trim().split('Heureka.cz » ')[1]
+    result.currency = "CZK";
+    
     Apify.pushData(result)
 };
